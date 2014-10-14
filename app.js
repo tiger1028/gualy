0f50ea429bd2a8a67e1a7b439ad5e8a5765f33c2
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-14 18:35:29 -0400

    Added a package file and made a express demo.

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Nothing here yet.');
+var express = require('express'),
+    app     = express();
+
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+});
+
+app.listen(3000);
+console.log('Started server on 0.0.0.0:3000');
