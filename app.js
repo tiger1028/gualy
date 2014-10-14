82313bbdc8eb2ff91ed6d0ab2d962e2dc3931373
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-14 18:39:55 -0400

    Added a header to the app.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var express = require('express'),
-    app     = express();
+// Name: app.js
+// Author: Cerek Hillen
+// Date Created: 10/14/2014
+// Date Changed: 10/14/2014
+//
+// Description:
+//   The main app file for the project. Sets everything up and starts the
+//   server.
+
+/////////////
+// Imports //
+var express = require('express');
+
+//////////
+// Code //
+var app = express();
 
 app.get('/', function (req, res) {
 	res.send('Hello world!');
