5b6fb463b7e6f2560e4b9ee37f92aebbc8776f5a
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-14 20:54:46 -0400

    Got everything rendering with a jade template.

--- app.js
+++ app.js
@@ -15,8 +15,24 @@ var express = require('express');
 // Code //
 var app = express();
 
+// Setting up Jade for the rendering engine.
+app.engine('.jade', require('jade').renderFile);
+
+// Serving static files.
+app.use('/static/', express.static(__dirname + '/static/'));
+
+// Serving the homepage.
 app.get('/', function (req, res) {
-	res.send('Hello world!');
+	res.render('home.jade', function (err, html) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).end();
+		} else {
+			res.status(200);
+			res.type('html');
+			res.send(html);
+		}
+	});
 });
 
 app.listen(3000);
