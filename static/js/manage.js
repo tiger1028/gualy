bc16bd2e00fa1e91ad44918983a8b1d92fb2b0c9
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-16 16:57:23 -0400

    Added some more stuff for the manage page. I need to write an API route for this.

---
+++ static/js/manage.js
@@ -0,0 +1,36 @@
+// Name        : manage.js
+// Author(s)   : Cerek Hillen
+// Date Created: 10/16/2014
+// Date Changed: 10/16/2014
+//
+// Description:
+//   Some front-end stuff for the manage page.
+
+//////////
+// Code //
+
+// Running the AJAX call to the server for putting a new goal.
+function pushGoal() {
+    var json = joinJSON($('#goalForm').serializeArray());
+    json.userId = $.cookie('logged');
+
+    $.ajax({
+        url: '/api/push/goal',
+        type: 'POST',
+        contentType: 'application/json;charset=UTF-8',
+        data: json
+    }).done(function (data) {
+        if (data.success)
+            successMessage(data.message);
+        else
+            dangerMessage(data.message);
+    });
+}
+
+// Page initialization code.
+$(document).ready(function () {
+    $('#goalForm').submit(function () {
+        pushGoal();
+        return false;
+    });
+});
