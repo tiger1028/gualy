38b6987f3420066d33d61866a7ae0bb9dd36ab6e
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-21 09:48:25 -0400

    Implemented the API endpoint for editing a goal.

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // Name        : app.js
 // Author(s)   : Cerek Hillen
 // Date Created: 10/15/2014
-// Date Changed: 10/20/2014
+// Date Changed: 10/21/2014
 //
 // Description:
 //   The main application file for the API sub-program.
@@ -13,6 +13,7 @@ var express    = require('express'),
     register   = require('./api/register.js')
     newgoal    = require('./api/newgoal.js')
     finishgoal = require('./api/finishgoal.js'),
+    editgoal   = require('./api/editgoal.js'),
     removegoal = require('./api/removegoal.js');
 
 //////////
@@ -26,6 +27,7 @@ app.post('/push/login/', login.post);
 app.post('/push/register/', register.post);
 app.post('/push/goal/', newgoal.post);
 app.post('/push/goal/finish/', finishgoal.post);
+app.post('/push/goal/edit/', finishgoal.post);
 app.post('/push/goal/remove/', removegoal.post);
 
 // Providing a JSON version of a 404.
---
+++ api/editgoal.js
@@ -0,0 +1,67 @@
+// Name        : editgoal.js
+// Author(s)   : Cerek Hillen
+// Date Created: 10/21/2014
+// Date Changed: 10/21/2014
+//
+// Description:
+//   The API endpoint for editing a goal.
+
+/////////////
+// Imports //
+var common = require('./common.js');
+
+//////////
+// Code //
+
+// The actual API endpoint.
+function post(req, res) {
+    if (req.cookies.logged === undefined) {
+        res.json({
+            success: false,
+            message: 'Must be logged in to edit one of your goals.'
+        });
+    } else {
+        if (common.hasAll(req.body, ['gid', 'newtext'])) {
+            schema.get.Goal.findOne({
+                _id: req.body.gid
+            }).exec(function (err, goal) {
+                if (err || goal === null) {
+                    res.json({
+                        success: false,
+                        message: 'Couldn\'t find your goal!'
+                    });
+                } else if (goal.completed) {
+                    res.json({
+                        success: false,
+                        message: 'You can\'t edit a goal you\'ve already finished!'
+                    });
+                } else if (req.cookies.logged == goal.userId) {
+                    goal.value = req.body.newText;
+
+                    goal.save(function (err) {
+                        if (err) {
+                            res.json({
+                                success: false,
+                                message: 'There was an error editing your goal!'
+                            });
+                        } else {
+                            res.json({
+                                success: true,
+                                message: 'Successfully edited your goal!'
+                            });
+                        }
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'You can\'t edit another person\'s goal!'
+                    });
+                }
+            });
+        }
+    }
+}
+
+/////////////
+// Exports //
+module.exports.post = post;
