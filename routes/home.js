dc48066c4f98a8a145d34897e7faa6028eb622e6
tiger1028 <gentleman.tiger1028@gmail.com>
2014-10-21 16:41:10 -0400

    Added a different spash page when you log in.

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 // Name        : home.js
 // Author(s)   : Cerek Hillen
 // Date Created: 10/14/2014
-// Date Changed: 10/16/2014
+// Date Changed: 10/21/2014
 //
 // Description:
 //   The route definitions for the homepage.
@@ -15,7 +15,10 @@ var renderer = require('../renderer.js');
 
 // Getting the homepage.
 function get(req, res) {
-    renderer.renderAndSend('home.jade', req, res, {});
+    if (req.cookies.logged === undefined)
+        renderer.renderAndSend('home.jade', req, res, {});
+    else
+        renderer.renderAndSend('loggedhome.jade', req, res, {});
 }
 
 /////////////
